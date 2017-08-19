9941d113179ea1cb2ca38cdf136bd1682ca66b8f
Yannick Boetzel <yannick.boetzel@gmail.com>
2017-08-19 15:39:21 +0200

    Reworked zfs_send_file & zfs_send_ssh functions.

--- pyznap/utils.py
+++ pyznap/utils.py
@@ -9,14 +9,17 @@ ZFS functions
 """
 
 import os
+import shutil
 
 from datetime import datetime
 from configparser import ConfigParser, NoOptionError
-import zfs
-from process import DatasetNotFoundError
-
 from subprocess import Popen, PIPE, CalledProcessError
 from getpass import getuser
+
+import paramiko as pm
+
+import zfs
+from process import DatasetNotFoundError
 
 
 def read_config(path):
@@ -179,105 +182,28 @@ def exists(executable=''):
     return bool(out)
 
 
-def zfs_list(filesystem='', recursive=True):
-    """Recusively lists filesystem. Returns false if filesystem does not exist."""
+def open_sftp(user, host, key=None, port=22):
+    """Opens an sftp connection to host"""
+    ssh = pm.SSHClient()
+    if not key:
+        key = '/home/{:s}/.ssh/id_rsa'.format(user)
+    if not os.path.isfile(key):
+        raise FileNotFoundError(key)
+    try:
+        ssh.load_system_host_keys('/home/{:s}/.ssh/known_hosts'.format(user))
+    except FileNotFoundError:
+        ssh.load_system_host_keys()
+    ssh.set_missing_host_key_policy(pm.WarningPolicy())
+    ssh.connect(hostname=host, port=port, username=user, key_filename=key)
 
-    assert isinstance(filesystem, str), "Input must be string."
-
-    cmd = ['zfs', 'list', '-o', 'name']
-
-    if recursive:
-        cmd.append('-r')
-    if filesystem:
-        cmd.append(filesystem)
-
-    proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
-    out, err = proc.communicate()
-
-    if not err:
-        out = out.splitlines()[1:]
-        out = [name.decode("utf-8") for name in out]
-        return out
-    else:
-        return False
+    assert ssh.get_transport().is_active(), 'Failed to connect to server'
+    return ssh, ssh.open_sftp()
 
 
-def zfs_list_snap(filesystem=''):
-    """Recusively lists snapshots. Returns false filesystem does not exist or there
-    are no snapshots."""
-
-    assert isinstance(filesystem, str), "Input must be string."
-
-    cmd = ['zfs', 'list', '-o', 'name', '-t', 'snap', '-r']
-
-    if filesystem:
-        cmd.append(filesystem)
-
-    proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
-    out, err = proc.communicate()
-
-    if not err:
-        out = out.splitlines()[1:]
-        out = [name.decode("utf-8") for name in out]
-        return out
-    else:
-        return False
-
-
-def zfs_snap(filesystem, snapname='', recursive=True):
-    """Takes a snapshot of a given filesystem. Returns false if filesystem does not exist."""
-
-    assert isinstance(filesystem, str), "Input must be string."
-    assert isinstance(snapname, str), "Input must be string."
-
-    if not zfs_list(filesystem, recursive):
-        return False
-
-    if not snapname:
-        today = datetime.today()
-        snapname = 'pyznap_{:s}'.format(today.strftime('%Y-%m-%d_%H:%M:%S'))
-
-    cmd = ['zfs', 'snapshot', '{:s}@{:s}'.format(filesystem, snapname)]
-
-    if recursive:
-        cmd.append('-r')
-
-    proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
-    _, err = proc.communicate()
-
-    if not err:
-        return zfs_list_snap(filesystem)
-    else:
-        return err
-
-
-def zfs_destroy(snapname, recursive=True):
-    """Destroys a filesystem or snapshot of a given filesystem.
-    Returns false if filesystem does not exist."""
-
-    assert isinstance(snapname, str), "Input must be string."
-
-    if not zfs_list(snapname, recursive):
-        return False
-
-    cmd = ['zfs', 'destroy', snapname]
-    if recursive:
-        cmd.append('-r')
-
-    proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
-    _, err = proc.communicate()
-
-    if not err:
-        return True
-    else:
-        return err
-
-
-def zfs_send_file(snapname, outfile='/tmp/pyznap.out', compress='lzop', mbuffer=True):
+def zfs_send_file(snapshot, base=None, intermediates=False, replicate=False,
+                  properties=False, deduplicate=False, outfile='/tmp/pyznap.out',
+                  compress='lzop', mbuffer=True):
     """Sends a snapshot to a file, with compression."""
-
-    if not zfs_list_snap(snapname):
-        raise ValueError('Snapshot does not exist.')
 
     if not os.path.isdir(os.path.dirname(outfile)):
         os.makedirs(os.path.dirname(outfile))
@@ -292,16 +218,11 @@ def zfs_send_file(snapname, outfile='/tmp/pyznap.out', compress='lzop', mbuffer=
     else:
         cmd_mbuffer = ['cat']
 
-    cmd_send = ['zfs', 'send', '-R', snapname]
-
     with open(outfile, 'w') as file:
-        proc_send = Popen(cmd_send, stdout=PIPE)
-        proc_mbuffer = Popen(cmd_mbuffer, stdin=proc_send.stdout, stdout=PIPE)
-        proc_compress = Popen(cmd_compress, stdin=proc_mbuffer.stdout, stdout=file)
-
-        proc_send.stdout.close()
-        proc_mbuffer.stdout.close()
-        _, err = proc_compress.communicate()
+        with snapshot.send(base=base, intermediates=intermediates, replicate=replicate,
+                           properties=properties, deduplicate=deduplicate) as send:
+            with Popen(cmd_mbuffer, stdin=send.stdout, stdout=PIPE) as buffer:
+                _, err = Popen(cmd_compress, stdin=buffer.stdout, stdout=file).communicate()
 
     if not err:
         return True
@@ -310,25 +231,17 @@ def zfs_send_file(snapname, outfile='/tmp/pyznap.out', compress='lzop', mbuffer=
         return False
 
 
-def zfs_send_ssh(snapname, outfile='/tmp/pyznap.out', compress='lzop', mbuffer=True,
-                 username=getuser(), hostname='localhost',
-                 keyfile=os.path.join(os.path.expanduser('~'), '.ssh/id_rsa')):
+def zfs_send_ssh(snapshot, user=getuser(), host='localhost', key=None, port=22,
+                 base=None, intermediates=False, replicate=False, properties=False,
+                 deduplicate=False, outfile='/tmp/pyznap.out', compress='lzop', mbuffer=True):
     """Sends a snapshot to a file, with compression."""
 
-    if hostname == 'localhost':
-        return zfs_send_file(snapname, outfile, compress, mbuffer)
+    if host == 'localhost':
+        return zfs_send_file(snapshot, base=base, intermediates=intermediates, replicate=replicate,
+                             properties=properties, deduplicate=deduplicate, outfile=outfile,
+                             compress=compress, mbuffer=mbuffer)
 
-    # Test ssh connection
-    cmd_ssh = ['ssh', '-i', keyfile, '{:s}@{:s}'.format(username, hostname)]
-    proc = Popen(cmd_ssh + ['exit'], stdout=PIPE, stderr=PIPE)
-    _, err = proc.communicate()
-    if err:
-        print('Cannot make ssh connection.')
-        print(err.decode('utf-8'))
-        return False
-
-    if not zfs_list_snap(snapname):
-        raise ValueError('Snapshot does not exist.')
+    ssh, sftp = open_sftp(user=user, host=host, key=key, port=port)
 
     if compress in ['lzop', 'gzip', 'pigz', 'lbzip2'] and exists(compress):
         cmd_compress = [compress, '-c']
@@ -340,22 +253,11 @@ def zfs_send_ssh(snapname, outfile='/tmp/pyznap.out', compress='lzop', mbuffer=T
     else:
         cmd_mbuffer = ['cat']
 
-    cmd_send = ['zfs', 'send', '-R', snapname]
-    cmd_ssh += ['cat - > {:s}'.format(outfile)]
+    with sftp.open(outfile, 'w') as file:
+        with snapshot.send(base=base, intermediates=intermediates, replicate=replicate,
+                           properties=properties, deduplicate=deduplicate) as send:
+            with Popen(cmd_mbuffer, stdin=send.stdout, stdout=PIPE) as buffer:
+                with Popen(cmd_compress, stdin=buffer.stdout, stdout=PIPE) as comp:
+                    shutil.copyfileobj(comp.stdout, file)
 
-    # Send snapshot over ssh
-    proc_send = Popen(cmd_send, stdout=PIPE)
-    proc_mbuffer = Popen(cmd_mbuffer, stdin=proc_send.stdout, stdout=PIPE)
-    proc_compress = Popen(cmd_compress, stdin=proc_mbuffer.stdout, stdout=PIPE)
-    proc_ssh = Popen(cmd_ssh, stdin=proc_compress.stdout, stdout=PIPE)
-
-    proc_send.stdout.close()
-    proc_mbuffer.stdout.close()
-    proc_compress.stdout.close()
-    _, err = proc_ssh.communicate()
-
-    if not err:
-        return True
-    else:
-        print(err.decode('utf-8'))
-        return False
+    return True
