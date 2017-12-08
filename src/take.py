99b451916b9aa5750081d938d31eaa9f04f0c600
Yannick Boetzel <yannick.boetzel@gmail.com>
2017-12-08 14:05:43 +0100

    take_config now checks if all subfilesystems have all snapshots. If a subfilesystem is added later it might not have full snapshots yet.

--- src/take.py
+++ src/take.py
@@ -117,9 +117,26 @@ def take_config(config):
             ssh = None
 
         try:
-            filesystem = zfs.open(fsname, ssh=ssh)
+            # filesystem = zfs.open(fsname, ssh=ssh)
+            # Children includes the base filesystem (filesystem)
+            children = zfs.find(path=fsname, types=['filesystem', 'volume'], ssh=ssh)
         except (ValueError, DatasetNotFoundError, CalledProcessError) as err:
             print('{:s} ERROR: {}'.format(logtime(), err))
             continue
 
-        take_snap(filesystem, conf)
+        # Take recursive snapshot of parent filesystem
+        take_snap(children[0], conf)
+        # Take snapshot of all children that don't have all snapshots yet
+        for child in children[1:]:
+            # Check if any of the parents (but child of base filesystem) have a config entry
+            for parent in children[1:]:
+                if ssh:
+                    parent_name = 'ssh:{:d}:{:s}@{:s}:{:s}'.format(port, user, host, parent.name)
+                else:
+                    parent_name = parent.name
+                # Skip if any parent entry already in config
+                if (child.name.startswith(parent.name) and
+                        parent_name in [entry['name'] for entry in config]):
+                    break
+            else:
+                take_snap(child, conf)
