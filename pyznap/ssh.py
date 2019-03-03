196e27736eb9fda8f6ef8729297db8f05108cc8f
Yannick Boetzel <boetzel@physik.uzh.ch>
2019-03-03 17:53:19 +0100

    Added compression option to ssh class.

--- pyznap/ssh.py
+++ pyznap/ssh.py
@@ -12,6 +12,7 @@ import os
 import logging
 import subprocess as sp
 from datetime import datetime
+from .utils import exists
 
 
 class SSHException(Exception):
@@ -40,7 +41,7 @@ class SSH:
         ssh command to use with subprocess
     """
 
-    def __init__(self, user, host, key=None, port=22):
+    def __init__(self, user, host, key=None, port=22, compress=None):
         """Initializes SSH class.
 
         Parameters
@@ -85,14 +86,57 @@ class SSH:
             sp.check_output(self.cmd + ['ls'], timeout=10, stderr=sp.PIPE)
         except sp.CalledProcessError as err:
             self.logger.error('Error while connecting to {:s}@{:s}: {}...'
-                              .format(self.user, self.host, err.stderr.decode()))
+                              .format(self.user, self.host, err.stderr.rstrip()))
             self.close()
-            raise SSHException(err.stderr.decode())
+            raise SSHException(err.stderr.rstrip())
         except sp.TimeoutExpired as err:
             self.logger.error('Error while connecting to {:s}@{:s}: {}...'
                               .format(self.user, self.host, err))
             self.close()
             raise SSHException(err)
+
+        # set up compression
+        self.setup_compress(compress)
+
+
+    def setup_compress(self, _type):
+        """Checks if compression algo is available on source and dest, creates cmd_compress command 
+        to use for send/recv.
+
+        Parameters
+        ----------
+        _type : {str}
+            Type of compression to use
+        """
+
+        self.compression = None
+        self.cmd_compress = self.cmd
+
+        if _type == None:
+            return
+
+        algos = ['gzip', 'lzop', 'bzip2', 'pigz', 'xz']
+        compress_cmd = {'gzip': 'gzip', 'lzop': 'lzop', 'bzip2': 'bzip2', 'pigz': 'pigz', 'xz': 'xz'}
+        decompress_cmd = {'gzip': 'gzip -d', 'lzop': 'lzop -d', 'bzip2': 'bzip2 -d', 'pigz': 'pigz -d', 
+                          'xz': 'xz -d'}
+
+        if _type not in algos:
+            self.logger.warning('Compression method {:s} not supported. Will continue without...'.format(_type))
+            return
+
+        # check if compression is available on source and dest
+        if not exists(_type):
+            self.logger.warning('Compression algo {:s} does not exist, continuing without compression...'
+                           .format(_type))
+            return
+        if not exists(_type, ssh=self):
+            self.logger.warning('Compression algo {:s} does not exist on {:s}@{:s}, continuing'
+                        'without compression...'.format(_type, self.user, self.host))
+            return
+
+        self.compression = _type
+        self.cmd_compress = [compress_cmd[_type]] + ['|'] + self.cmd + [decompress_cmd[_type]] + ['|']
+
 
     def close(self):
         """Closes the ssh connection by invoking '-O exit' (deletes socket file)"""
@@ -101,6 +145,7 @@ class SSH:
             sp.check_output(self.cmd + ['-O', 'exit'], timeout=5, stderr=sp.PIPE)
         except (sp.CalledProcessError, sp.TimeoutExpired):
             pass
+
 
     def __del__(self):
         self.close()
